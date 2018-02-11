dcb2c6de1b0a485fc50f5bafa8675e43f167e831
Carl Findahl <sportleroro@gmail.com>
2018-02-11 22:18:19 +0100

    Expand textures to support mipmapping and change of repeat/filter modes
    Textures support auto-loading of mipmaps following a strict naming conventions. When mipmaps are not found, OpenGL is asked to auto-generate the requested mipmaps instead. Repeat mode is set for both S and T coordinates. Filtering is set for minification only. For magnification it will always be linear.

--- include/texture.h
+++ include/texture.h
@@ -6,10 +6,41 @@
 #include <string>
 
 
+// Texture repeat modes
+enum class ETextureRepeatMode
+{
+	Repeat,
+	MirrorRepeat,
+	ClampToEdge,
+	ClampToBorder,
+	MirroredClampToEdge
+};
+
+
+// Texture filtering modes
+enum class ETextureFilterMode
+{
+	NearestNearest,
+	NearestLinear,
+	LinearNearest,
+	Trilinear	
+};
+
+
+/*
+ * For loading and binding Textures to the OpenGL context.
+ * Supports mipmap loading. Specify levels with level parameter.
+ * Currently only does PNG loading.
+ *
+ * File naming convention is:
+ *	MIP 0: filename.png
+ *	MIP 1: filename_1.png
+ *	MIP N: filename_N.png
+ */
 class Texture
 {
 public:
-	Texture(const std::string& filepath);
+	Texture(const std::string& filepath, unsigned levels = 1);
 	~Texture();
 
 	// Bind texture to the provided bindingPoint [0-32]
@@ -17,6 +48,12 @@ public:
 
 	// Unbind from the last bound binding point
 	void unbind() const;
+
+	// Set the repeat mode of the texture
+	void setRepeatMode(ETextureRepeatMode mode);
+
+	// Set the minification filter for the texture
+	void setFilterMode(ETextureFilterMode mode);
 
 	// Get the OpenGL name of the texture
 	const unsigned name() const { return mName; }
@@ -29,9 +66,11 @@ private:
 	// OpenGL name
 	unsigned mName = 0;
 
-	// Bound to binding point
-	mutable unsigned mBindingPoint = 0;
+	// MipMap Levels
+	unsigned mLevels = 0;
 
+	// Bound to binding point
+	mutable unsigned mBindingPoint = 0;	
 };
 
 
--- src/texture.cpp
+++ src/texture.cpp
@@ -1,16 +1,22 @@
 #include "texture.h"
 #include "gl_cpp.hpp"
 #include "ext/stb_image.h"
+#include "logging.h"
+#include <experimental/filesystem>
 
+using namespace std::experimental;
 
-Texture::Texture(const std::string& filepath)
+Texture::Texture(const std::string& filepath, unsigned levels/*=1*/) : mLevels(levels)
 {
 	gl::CreateTextures(gl::TEXTURE_2D, 1, &mName);
-	gl::TextureParameteri(mName, gl::TEXTURE_MIN_FILTER, gl::LINEAR);
+	gl::TextureParameteri(mName, gl::TEXTURE_MIN_FILTER, gl::LINEAR_MIPMAP_LINEAR); // Trilinear
 	gl::TextureParameteri(mName, gl::TEXTURE_MAG_FILTER, gl::LINEAR);
 	gl::TextureParameteri(mName, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE);
 	gl::TextureParameteri(mName, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE);
-	loadFromFile(filepath);
+
+	// Only load if it mipmap levels are valid
+	if (levels == 0) logErr("Texture mipmap levels can not be 0!");
+	else loadFromFile(filepath);
 }
 
 Texture::~Texture()
@@ -29,11 +35,86 @@ void Texture::unbind() const
 	gl::BindTextureUnit(mBindingPoint, 0);
 }
 
+void Texture::setRepeatMode(ETextureRepeatMode mode)
+{
+	switch (mode)
+	{
+	case ETextureRepeatMode::Repeat:
+		gl::TextureParameteri(mName, gl::TEXTURE_WRAP_S, gl::REPEAT);
+		gl::TextureParameteri(mName, gl::TEXTURE_WRAP_T, gl::REPEAT);
+		break;
+	case ETextureRepeatMode::MirrorRepeat:
+		gl::TextureParameteri(mName, gl::TEXTURE_WRAP_S, gl::MIRRORED_REPEAT);
+		gl::TextureParameteri(mName, gl::TEXTURE_WRAP_T, gl::MIRRORED_REPEAT);
+		break;
+	case ETextureRepeatMode::ClampToEdge:
+		gl::TextureParameteri(mName, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE);
+		gl::TextureParameteri(mName, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE);
+		break;
+	case ETextureRepeatMode::ClampToBorder:
+		gl::TextureParameteri(mName, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_BORDER);
+		gl::TextureParameteri(mName, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_BORDER);
+		break;
+	case ETextureRepeatMode::MirroredClampToEdge:
+		gl::TextureParameteri(mName, gl::TEXTURE_WRAP_S, gl::MIRROR_CLAMP_TO_EDGE);
+		gl::TextureParameteri(mName, gl::TEXTURE_WRAP_T, gl::MIRROR_CLAMP_TO_EDGE);
+		break;
+	default:
+		break;
+	}
+}
+
+void Texture::setFilterMode(ETextureFilterMode mode)
+{
+	switch (mode)
+	{
+	case ETextureFilterMode::NearestNearest:
+		gl::TextureParameteri(mName, gl::TEXTURE_MIN_FILTER, gl::NEAREST_MIPMAP_NEAREST);
+		break;
+	case ETextureFilterMode::NearestLinear:
+		gl::TextureParameteri(mName, gl::TEXTURE_MIN_FILTER, gl::NEAREST_MIPMAP_LINEAR);
+		break;
+	case ETextureFilterMode::LinearNearest:
+		gl::TextureParameteri(mName, gl::TEXTURE_MIN_FILTER, gl::LINEAR_MIPMAP_NEAREST);
+		break;
+	case ETextureFilterMode::Trilinear:
+		gl::TextureParameteri(mName, gl::TEXTURE_MIN_FILTER, gl::LINEAR_MIPMAP_LINEAR);
+		break;
+	default:
+		break;
+
+	}
+}
+
 void Texture::loadFromFile(const std::string& filepath)
 {
 	int w, h, comp;
-	unsigned char* imageData = stbi_load(filepath.c_str(), &w, &h, &comp, STBI_rgb_alpha);
-	gl::TextureStorage2D(mName, 1, gl::RGBA8, w, h);
-	gl::TextureSubImage2D(mName, 0, 0, 0, w, h, gl::RGBA, gl::UNSIGNED_BYTE, imageData);
-	stbi_image_free(imageData);
+	
+	// Always load MipMap level 0
+	unsigned char* mipBase = stbi_load(filepath.c_str(), &w, &h, &comp, STBI_rgb_alpha);
+	gl::TextureStorage2D(mName, mLevels, gl::RGBA8, w, h);
+	gl::TextureSubImage2D(mName, 0, 0, 0, w, h, gl::RGBA, gl::UNSIGNED_BYTE, mipBase);
+	
+	// Load any further MipMaps
+	for (unsigned i = 1; i < mLevels; ++i)
+	{
+		// Find new path based on naming convention
+		const std::string mipPath = filepath.substr(0, filepath.find(".png")) + "_" + std::to_string(i) + ".png";
+		if (filesystem::v1::exists(mipPath))
+		{
+			// Load and send data to OpenGL
+			unsigned char* mipData = stbi_load(mipPath.c_str(), &w, &h, &comp, STBI_rgb_alpha);
+			gl::TextureSubImage2D(mName, i, 0, 0, w, h, gl::RGBA, gl::UNSIGNED_BYTE, mipData);
+			stbi_image_free(mipData);
+		}
+		else
+		{
+			// If path is invalid, then rely on auto-generated mip maps
+			logCustom()->warn("Failed to load: {} auto-generating mip maps instead!", mipPath);
+			gl::GenerateTextureMipmap(mName);
+			break;
+		}
+	}
+	
+	stbi_image_free(mipBase);
 }
