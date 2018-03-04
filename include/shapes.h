4cec05856d3979a3b1f2132f348f80e638a58d54
Carl Findahl <sportleroro@gmail.com>
2018-03-04 13:58:09 +0100

    Add shape class to define basic GL primitives

---
+++ include/shapes.h
@@ -0,0 +1,87 @@
+/// OpenGL - by Carl Findahl - 2018
+
+/* 
+ * Shapes contains classes to easily create
+ * various shape primitives in OpenGL.
+ */
+
+#ifndef SHAPES_H
+#define SHAPES_H
+
+#include "vertex.h"
+#include "buffer.h"
+#include "linalg.h"
+#include "vertexArray.h"
+
+#include <vector>
+#include <memory>
+#include <utility>
+
+struct ShapeGLData
+{
+    ShapeGLData(VertexBuffer&& vb, IndexBuffer&& ib, VertexArray&& va) : vbo(std::move(vb)), ibo(std::move(ib)), vao(std::move(va)) {}
+
+    VertexBuffer vbo;
+    IndexBuffer ibo;
+    VertexArray vao;
+};
+
+class Shape2D
+{
+public:
+    virtual ~Shape2D() noexcept = default;
+
+    // Bind to render
+    void bind() const;
+
+    // Unbind
+    void unbind() const;
+
+    // Get the number of indices this shape requires
+    const unsigned getIndexCount() const;
+
+protected:
+    // Add a vertex to the shape
+    void addVertex(const vec2& pos, const vec3& col, const vec2& tc);
+
+    // Add an index to be rendered
+    void addIndex(const unsigned idx);
+
+    // Add a series of provided indices to be rendered
+    template<typename... Is>
+    void addIndices(const Is... i);
+
+    // Call after vertices/indices are added to commit data to OpenGL
+    void init();
+
+private:
+    // Vertices
+    std::vector<Vertex> mVertices;
+
+    // Indices
+    std::vector<unsigned> mIndices;
+
+    // GL Data
+    std::unique_ptr<ShapeGLData> mGLData = nullptr;
+};
+
+template<typename... Is>
+void Shape2D::addIndices(const Is... i)
+{
+    (mIndices.push_back(i), ...);
+}
+
+class Quad : public Shape2D
+{
+public:
+    Quad(const vec2& size, const vec3& col);
+
+};
+
+class Circle : public Shape2D
+{
+public:
+    Circle(const float radius, const unsigned points, const vec3& col);
+};
+
+#endif // SHAPES_H
---
+++ src/shapes.cpp
@@ -0,0 +1,109 @@
+#include "shapes.h"
+#include "logging.h"
+
+#include <cmath>
+
+void Shape2D::bind() const
+{
+    if (!mGLData)
+    {
+        logErr("Shape2D does not have any data");
+        return;
+    }
+
+    mGLData->vao.bind();
+}
+
+void Shape2D::unbind() const
+{
+    if (!mGLData)
+    {
+        logErr("Shape2D does not have any data");
+        return;
+    }
+
+    mGLData->vao.unbind();
+}
+
+const unsigned Shape2D::getIndexCount() const
+{
+    if (mGLData)
+    {
+        return mGLData->ibo.getCount();
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void Shape2D::addVertex(const vec2& pos, const vec3& col, const vec2& tc)
+{
+    mVertices.push_back({ pos[0], pos[1], 0.f, col[0], col[1], col[2], tc[0], tc[1] });
+}
+
+void Shape2D::addIndex(const unsigned idx)
+{
+    mIndices.push_back(idx);
+}
+
+void Shape2D::init()
+{
+    VertexBuffer vbo(mVertices.data(), mVertices.size() * sizeof(Vertex));
+    IndexBuffer ibo(mIndices.data(), mIndices.size() * sizeof(unsigned), static_cast<unsigned>(mIndices.size()));
+    
+    VertexArray vao(vbo, ibo);
+    vao.addAttribute(3, gl::FLOAT, offsetof(Vertex, x), false);
+    vao.addAttribute(3, gl::FLOAT, offsetof(Vertex, r), false);
+    vao.addAttribute(2, gl::FLOAT, offsetof(Vertex, u), false);
+
+    mGLData = std::make_unique<ShapeGLData>(std::move(vbo), std::move(ibo), std::move(vao));
+}
+
+//////
+/// RECTANGLE
+//////
+
+Quad::Quad(const vec2& size, const vec3& col)
+{
+    addVertex(size / -2.f, col, vec2{ 0.f, 0.f });
+    addVertex(vec2{ size[0] / 2.f, size[1] / -2.f }, col, vec2{ 1.f, 0.f });
+    addVertex(size / 2.f, col, vec2{ 1.f, 1.f });
+    addVertex(vec2{ size[0] / -2.f, size[1] / 2.f }, col, vec2{ 0.f, 1.f });
+
+    addIndices(0, 1, 2, 2, 3, 0);
+
+    init();
+}
+
+Circle::Circle(const float radius, const unsigned points, const vec3& col)
+{
+    // The center point
+    addVertex(vec2(0.f, 0.f), col, vec2(0.5f, 0.5f));
+
+    // #TODO Create a proper math header with consts like this
+    constexpr float PI = 3.14156f;
+
+    // Loop around the circle
+    for (int i = 0; i <= points; ++i)
+    {
+        // Add x-Coordinate
+        auto x = std::cos((2 * PI / points) * i) * (radius * 2);
+        auto y = std::sin((2 * PI / points) * i) * (radius * 2);
+
+        // Add Texture Coordinates
+        auto u = std::cos(((2 * PI / points) * i) + 1.f) / 2.f;
+        auto v = std::sin(((2 * PI / points) * i) + 1.f) / 2.f;
+
+        addVertex(vec2(x, y), col, vec2(u, v));
+    }
+
+    // Set up index buffer by walking the circle and stepping into the center for each triangle
+    std::vector<unsigned> indices;
+    for (int i = 0; i <= points; ++i)
+    {
+        addIndices(0, i + 2, i + 1);
+    }
+
+    init();
+}
