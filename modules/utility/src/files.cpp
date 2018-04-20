2c3f6b31baf30355bfe4e9317e070f00091c3c22
Carl Findahl <sportleroro@gmail.com>
2018-04-20 15:29:04 +0200

    Add resource path printing in debug mode

--- modules/utility/src/files.cpp
+++ modules/utility/src/files.cpp
@@ -8,11 +8,11 @@
 using namespace std::experimental; // For filesystem access
 
 const std::string readFile(const std::string& filepath) {
-	std::stringstream out;
-	std::ifstream file(filepath);
-	if (file.is_open()) {
-		out << file.rdbuf();
-		return out.str();
+    std::stringstream out;
+    std::ifstream file(filepath);
+    if (file.is_open()) {
+        out << file.rdbuf();
+        return out.str();
     }
     else
     {
@@ -24,5 +24,11 @@ const std::string readFile(const std::string& filepath) {
 std::string getResourcePath(const std::string& resourceName)
 {
     // Return the path to the resource in an OS-Agnostic way
+#ifndef NDEBUG
+    auto p = filesystem::v1::current_path().append("res").append(resourceName).string();
+    logInfo("Getting resource path: {}", p);
+    return p;
+#else
     return filesystem::v1::current_path().append("res").append(resourceName).string();
+#endif
 }
